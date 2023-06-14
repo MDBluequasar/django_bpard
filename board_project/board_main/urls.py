275eb32504b6eb0d647fc8753e94dc5b60879ba9
MDBluequasar <dlghkswls112@naver.com>
2023-06-14 18:28:52 +0900

    sre

--- board_project/board_main/urls.py
+++ board_project/board_main/urls.py
@@ -1,13 +1,29 @@
 from django.urls import path
 from . import views_test
 from django.urls import include
+from . import views
 
 urlpatterns = [           
-    path('', views_test.test_html_multi_data),
-    path('test_json', views_test.test_json_data),
-    path('parameter_data', views_test.test_html_parameter_data),
-    path('parameter_data2/<int:my_id>', views_test.test_html_parameter_data2),
-    # path('test_post', views_test.test_post_form),
-    path('test_post_handle', views_test.test_post_handle),
+    # path('', views_test.test_html_multi_data),
+    # path('test_json', views_test.test_json_data),
+    # path('parameter_data', views_test.test_html_parameter_data),
+    # path('parameter_data2/<int:my_id>', views_test.test_html_parameter_data2),
+    # # path('test_post', views_test.test_post_form),
+    # path('test_post_handle', views_test.test_post_handle),
+    # path('test_select_one/<int:my_id>', views_test.test_select_one),
+    # #호출 http://localhost:8000/test_select_one/1(데이터의 id번호)
+    # path('test_select_all', views_test.test_select_all),
+    # path('test_select_filter', views_test.test_select_filter),
+    # # http://localhost:8000/test_select_filter?name=XXXX
+    # path('test_update', views_test.test_update),
     
+    path('',views.home),
+    path('authors/', views.author_list),
+    path('author/new', views.author_new),
+    path('author/<int:my_id>', views.author_detail),
+    path('author/<int:my_id>/update', views.author_update),
+    path('posts/', views.post_list),
+    
+    
+
 ]
--- board_project/board_main/views_test.py
+++ board_project/board_main/views_test.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
+from .models import Test
 # get 요청 시 html file 그대로 return
 def test_html(request):
     return render(request, 'test/test.html')
@@ -61,13 +62,83 @@ def test_html_parameter_data2(request, my_id):
 #     return render(request, 'test/test_post_form.html')
 
 def test_post_handle(request):
-    if request.method == 'post':
-        name = request.POST['my_name']
-        email = request.POST['my_email']
-        password = request.POST['my_password']
-        print(name) 
-        print(email) 
-        print(password)
+    if request.method == 'POST':
+        my_name = request.POST['my_name']
+        my_email = request.POST['my_email']
+        my_password = request.POST['my_password']
+
+        # DB에 insert -> save함수 사용
+        # DB의 table 과 sync 가 맞는 test class에서 객체를 만들어 save
+        t1 =Test()
+        t1.name = my_name
+        t1.email = my_email
+        t1.password = my_password
+        t1.save()
+
         return redirect('/') # return HttpResponse('ok') 는 http 응답 시 화면처리 없을 때
     else:
         return render(request, 'test/test_post_form.html')  # 위 함수 하나로 합친 코드      
+
+
+def test_select_one(request, my_id):
+    # 단 건만을 조회할 땐 get() 함수 사용
+    t1 = Test.objects.get(id = my_id)
+    return render(request, 'test/test_select_one.html', {'data':t1})
+
+def test_select_all(request):
+    # 모든 data 조회 : select * from XXXXX ; all()함수 사용
+    tests = Test.objects.all()
+    # for a in tests:
+    #     print(a.name)
+    #     print(a.email)
+    #     print(a.password)
+
+    return render(request, 'test/test_select_all.html', {'datas' : tests})
+
+# where 조건으로 다건을 조회할 땐 filter() 함수 사용 (중복데이터)
+def test_select_filter(request):
+    my_name = request.GET.get('name')
+    tests = Test.objects.filter(name = my_name)
+
+    return render(request, 'test/test_select_filter.html', {'datas' : tests})
+# 수정
+# update를 하기위해서는 해당건으로 사전엔 조회하기 위한 id값이 필요
+# method는 등록과 동일하게 save()함수 사용 
+# save함수는 신규객체를 save하면 insert, 기존객체를 save하면 update
+def test_update(request):
+    if request.method == 'POST':
+        my_id = request.POST['my_id']
+        t1 = Test.objects.get(id=my_id)
+        my_name = request.POST['my_name']
+        my_email = request.POST['my_email']
+        my_password = request.POST['my_password']
+        print(type(my_id))
+        t1 =Test()
+        t1.name = my_name
+        t1.email = my_email
+        t1.password = my_password
+        t1.save()
+
+        return redirect('/') 
+    else:
+        return render(request, 'test/test_update.html')
+    
+# 삭제는 delete()함수 사용. update와 마찬가지로 기존객체 조회후 delete
+
+# def test_delete(request):
+#     if request.method == 'POST':
+#         my_id = request.POST['my_id']
+#         t1 = Test.objects.get(id=my_id)
+#         my_name = request.POST['my_name']
+#         my_email = request.POST['my_email']
+#         my_password = request.POST['my_password']
+#         print(type(my_id))
+#         t1 =Test()
+#         t1.name = my_name
+#         t1.email = my_email
+#         t1.password = my_password
+#         t1.delete()
+
+#         return redirect('/') 
+#     else:
+#         return render(request, 'test/test_update.html')
